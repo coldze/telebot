3d8ddfd635abe6dc90a47cb0ee488ab0fb6e4671
coldze <postbox4me@mail.ru>
2016-04-26 10:58:41 +0300

    Added command-handlers Added default update callback

---
+++ bot_handlers.go
@@ -0,0 +1,49 @@
+package telebot
+
+import (
+	"fmt"
+	"github.com/coldze/telebot/send"
+	"github.com/coldze/telebot/receive"
+	"errors"
+)
+
+type CommandCallType struct {
+	Argument string
+	MetaInfo *receive.UpdateType
+}
+
+type CommandHandler func(command *CommandCallType) (*send.SendType, error)
+type MessageHandler func(message *receive.UpdateType) (*send.SendType, error)
+
+type BotHandlers struct {
+	handlers  map[string]CommandHandler
+	onMessage MessageHandler
+}
+
+func (r *BotHandlers) RegisterCommand(commandName string, handler CommandHandler) error {
+	_, ok := r.handlers[commandName]
+	if ok {
+		return fmt.Errorf("Command handler already registered: %s.", commandName)
+	}
+	r.handlers[commandName] = handler
+	return nil
+}
+
+func (r *BotHandlers) OnCommand(commandName string, args *CommandCallType) (*send.SendType, error) {
+	handler, ok := r.handlers[commandName]
+	if !ok {
+		return nil, fmt.Errorf("Handler not set for command: %s.", commandName)
+	}
+	return handler(args)
+}
+
+func (r *BotHandlers) OnMessage(message *receive.UpdateType) (*send.SendType, error) {
+	if r.onMessage == nil {
+		return nil, errors.New("No default handler.")
+	}
+	return r.onMessage(message)
+}
+
+func NewBotHandlers(onMessage MessageHandler) *BotHandlers {
+	return &BotHandlers{onMessage: onMessage, handlers: make(map[string]CommandHandler)}
+}
---
+++ default_update_callback.go
@@ -0,0 +1,50 @@
+package telebot
+
+import (
+	"github.com/coldze/telebot/receive"
+	"strings"
+	"github.com/coldze/telebot/send"
+	"errors"
+)
+
+func parseCommand(update *receive.UpdateType) (*CommandCallType, string) {
+	for index := range update.Message.Entities {
+		if update.Message.Entities[index].Type != receive.ENTITY_TYPE_BOT_COMMAND {
+			continue
+		}
+		var cmd CommandCallType
+		cmdName := update.Message.Text[update.Message.Entities[index].Offset:update.Message.Entities[index].Length]
+		argsStart := update.Message.Entities[index].Offset + update.Message.Entities[index].Length
+		if argsStart < int64(len(update.Message.Text)) {
+			cmd.Argument = strings.TrimSpace(update.Message.Text[argsStart:])
+		}
+		cmd.MetaInfo = update
+		return &cmd, cmdName
+	}
+	return nil, ""
+}
+
+func NewDefaultUpdateCallback(logger Logger, handlers *BotHandlers) (UpdateCallback, error) {
+	if logger == nil {
+		return nil, errors.New("Invalid logger specified.")
+	}
+	if handlers == nil {
+		return nil, errors.New("Invalid handlers specified.")
+	}
+	return func (update *receive.UpdateType) (*send.SendType, error) {
+		if update == nil {
+			return nil, nil
+		}
+		if update.Message == nil {
+			return nil, nil
+		}
+		if update.Message.Entities == nil {
+			return handlers.OnMessage(update)
+		}
+		cmd, cmdName := parseCommand(update)
+		if cmd == nil {
+			return handlers.OnMessage(update)
+		}
+		return handlers.OnCommand(cmdName, cmd)
+	}, nil
+}
